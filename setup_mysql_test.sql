118a01f632290c7c8495687373976ea7c6a6fef7
Kis123mas <emmanuelezekielsolomon@gmail.com>
2022-12-09 18:12:12 +0100

    all files fixed

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,8 +1,11 @@
--- create a DB
--- name hbnb_dev_db
+-- prepares a MySQL server for the project
+-- GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
-SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
+USE hbnb_test_db;
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+USE performance_schema;
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
+SET FOREIGN_KEY_CHECKS=1;
